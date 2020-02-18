e0a8dc7b7562fad4792ead105fc684d5cecc9109
Anatoly Rodionov <arodionov@nexvortex.com>
2020-02-18 16:22:14 -0500

    initial commit

---
+++ src/lstree.erl
@@ -0,0 +1,128 @@
+%-------------------------------------------------------------------------------------------------
+
+-module(lstree).
+
+%-------------------------------------------------------------------------------------------------
+
+-export([from_list/1, from_list/2, to_list/1]).
+-export([min/1, max/1]).
+-export([min_max/1, max_min/1]).
+-export([min_max_p/1, max_min_p/1]).
+-export([map/2, map/3, map/4]).
+
+%-------------------------------------------------------------------------------------------------
+
+-define(ALPHA_MAX, 16#7FFFFFFFFFFFFFF).   % Erlang max integer (60 bit)
+-define(BETA_MIN,  -?ALPHA_MAX).
+
+%-------------------------------------------------------------------------------------------------
+
+-record(lsnode,{
+	level    = 0      :: non_neg_integer(),
+	score    = false  :: integer() | false,
+	children = []     :: [lsnode()]   
+	}).
+-type lsnode()       :: #lsnode{}. 
+
+%-------------------------------------------------------------------------------------------------
+-spec from_list([number()] | number()) -> lsnode().
+
+from_list(Arg) -> ?MODULE:from_list(Arg, 0).
+
+%-------------------------------------------------------------------------------------------------
+-spec from_list([number()] | number(), non_neg_integer()) -> lsnode().
+
+from_list(S, L) when is_list(S) -> 
+	#lsnode{children = [?MODULE:from_list(A, L + 1) || A <- S], level = L};
+from_list(S, L) when is_number(S) -> #lsnode{score = S, level = L}.
+
+%-------------------------------------------------------------------------------------------------
+-spec to_list(lsnode()) -> [term()].
+
+to_list(Anode) when Anode#lsnode.children == [] -> Anode#lsnode.score;
+to_list(Anode) -> [?MODULE:to_list(A) || A <- Anode#lsnode.children].
+
+%-------------------------------------------------------------------------------------------------
+-spec max(lsnode() | [number()]) -> integer().
+
+max(A) when is_list(A) -> ?MODULE:max(?MODULE:from_list(A));
+max(A) -> ?MODULE:map(A, fun lists:max/1).
+
+%-------------------------------------------------------------------------------------------------
+-spec min(lsnode() | [number()]) -> integer().
+
+min(A) when is_list(A) -> ?MODULE:min(?MODULE:from_list(A));
+min(A) -> ?MODULE:map(A, fun lists:min/1).
+
+%-------------------------------------------------------------------------------------------------
+-spec max_min(lsnode() | [number()]) -> integer().
+
+max_min(A) when is_list(A) -> ?MODULE:max_min(?MODULE:from_list(A));
+max_min(A) -> ?MODULE:map(A, fun lists:max/1, fun lists:min/1).
+
+%-------------------------------------------------------------------------------------------------
+-spec min_max(lsnode() | [number()]) -> integer().
+
+min_max(A) when is_list(A) -> ?MODULE:min_max(?MODULE:from_list(A));
+min_max(A) -> ?MODULE:map(A, fun lists:min/1, fun lists:max/1).
+
+%-------------------------------------------------------------------------------------------------
+-spec max_min_p(lsnode() | [number()]) -> integer().
+
+max_min_p(A) when is_list(A) -> max_min_p(?MODULE:from_list(A));
+max_min_p(A) -> ?MODULE:map(A, fun pmax/3, fun pmin/3, {?BETA_MIN, ?ALPHA_MAX}).
+
+%-------------------------------------------------------------------------------------------------
+-spec min_max_p(lsnode() | [number()]) -> integer().
+
+min_max_p(A) when is_list(A) -> min_max_p(?MODULE:from_list(A));
+min_max_p(A) -> ?MODULE:map(A, fun pmin/3, fun pmax/3, {?BETA_MIN, ?ALPHA_MAX}).
+
+%-------------------------------------------------------------------------------------------------
+-spec map(lsnode(), fun()) -> integer() | [integer()].
+
+map(Anode, _Fn) when Anode#lsnode.children == [] -> Anode#lsnode.score;
+map(Anode, Fn) -> Fn([?MODULE:map(A, Fn) || A <- Anode#lsnode.children]).
+
+%-------------------------------------------------------------------------------------------------
+-spec map(lsnode(), fun(), fun()) -> integer() | [integer()].
+
+map(Anode, _Fa, _Fb) when Anode#lsnode.children == [] -> Anode#lsnode.score;
+map(Anode, Fa, Fb) -> Fa([?MODULE:map(A, Fb, Fa) || A <- Anode#lsnode.children]).
+
+%-------------------------------------------------------------------------------------------------
+-spec map(lsnode(), fun(), fun(), {number(), number()}) -> integer() | [integer()].
+
+map(Anode, _Fa, _Fb, _) when Anode#lsnode.children == [] -> Anode#lsnode.score;
+map(Anode, Fa, Fb, {Alpha, Beta}) -> 
+	Fa( fun(N, {A, B}) -> ?MODULE:map(N, Fb, Fa, {A, B}) end,
+		{Alpha, Beta},
+		Anode#lsnode.children).
+
+%-------------------------------------------------------------------------------------------------
+-spec pmax(fun(), {number(), number()}, [lsnode()]) -> number().
+pmax(F, {AL, BE}, Lst) -> pmax(F, {AL, BE}, Lst, ?BETA_MIN). 
+
+-spec pmax(fun(), {number(), number()}, [lsnode()], number()) -> number().
+pmax(_F, _, [], X) ->  X;
+pmax(F, {Al, Be}, [N | Tail], X) -> 
+	A =  erlang:max(F(N, {Al, Be}), X),
+	case A < Be of
+		true  -> pmax(F, {A, Be}, Tail, A);
+		false -> A      % pruning
+	end.
+
+%-------------------------------------------------------------------------------------------------
+-spec pmin(fun(), {number(), number()}, [lsnode()]) -> number().
+pmin(F, {AL, BE}, Lst) -> pmin(F, {AL, BE}, Lst, ?ALPHA_MAX). 
+
+-spec pmin(fun(), {number(), number()}, [lsnode()], number()) -> number().
+pmin(_F, _, [], X) ->  X;
+pmin(F, {Al, Be}, [N | Tail], X) -> 
+	B =  erlang:min(F(N, {Al, Be}), X),
+	case Al < B of
+		true  -> pmin(F, {Al, B}, Tail, B);
+		false -> B      % pruning
+	end.
+
+%-------------------------------------------------------------------------------------------------
---
+++ test/lstree_tests.erl
@@ -0,0 +1,144 @@
+
+%-------------------------------------------------------------------------------------------------
+
+-module(lstree_tests).
+
+%-------------------------------------------------------------------------------------------------
+
+-include_lib("eunit/include/eunit.hrl").
+
+%-----------------------------------------------------------------------------------------------
+
+-record(lsnode,{
+    level    = 0      :: non_neg_integer(),
+    score    = false  :: integer() | false,
+    children = []     :: [lsnode()]   
+    }).
+-type lsnode()       :: #lsnode{}. 
+
+%-----------------------------------------------------------------------------------------------
+
+mk_test() ->
+    L = example(0),
+	T = {lsnode,0,false,
+        [{lsnode,1,1,[]},
+         {lsnode,1,2,[]},
+         {lsnode,1,3,[]},
+         {lsnode,1,false,
+                 [{lsnode,2,4,[]},{lsnode,2,false,[{lsnode,3,-5,[]}]}]},
+         {lsnode,1,6,[]}]},
+	?assertMatch(T, lstree:from_list(L)),
+	?assertMatch(L, lstree:to_list(T)),
+    ok.
+
+%-----------------------------------------------------------------------------------------------
+
+max_test() ->
+    T0 = lstree:from_list(example(0)),
+    ?assertMatch( 6, lstree:max(T0)),
+    ?assertMatch(-5, lstree:min(T0)),
+
+    T1 = lstree:from_list(example(1)),
+    ?assertMatch( 9, lstree:max(T1)),
+    ?assertMatch( 3, lstree:min(T1)),
+
+    T2 = lstree:from_list(example(2)),
+    ?assertMatch(25, lstree:max(T2)),
+    ?assertMatch( 0, lstree:min(T2)),
+
+    T3 = lstree:from_list(example(3)),
+    ?assertMatch( 9, lstree:max(T3)),
+    ?assertMatch( 1, lstree:min(T3)),
+    ok.
+
+%-----------------------------------------------------------------------------------------------
+
+max_min_test() ->
+    ?assertMatch( 6, lstree:max_min(example(0))),
+    ?assertMatch( 1, lstree:min_max(example(0))),
+
+    T1 = lstree:from_list(example(1)),
+    ?assertMatch( 6, lstree:max_min(T1)),
+    ?assertMatch( 6, lstree:min_max(T1)),
+
+    T2 = lstree:from_list(example(2)),
+    ?assertMatch( 3, lstree:max_min(T2)),
+    ?assertMatch(14, lstree:min_max(T2)),
+
+    T3 = lstree:from_list(example(3)),
+    ?assertMatch( 5, lstree:max_min(T3)),
+    ?assertMatch( 3, lstree:min_max(T3)),
+
+    ok.
+
+%-----------------------------------------------------------------------------------------------
+
+max_min_p_test() ->
+    ?assertMatch( 6, lstree:max_min(example(0))),
+    ?assertMatch( 1, lstree:min_max(example(0))),
+
+    T1 = lstree:from_list(example(1)),
+    ?assertMatch( 6, lstree:max_min_p(T1)),
+    ?assertMatch( 6, lstree:min_max_p(T1)),
+
+    T2 = lstree:from_list(example(2)),
+    ?assertMatch( 3, lstree:max_min_p(T2)),
+    ?assertMatch(14, lstree:min_max_p(T2)),
+
+    T3 = lstree:from_list(example(3)),
+    ?assertMatch( 5, lstree:max_min_p(T3)),
+    ?assertMatch( 3, lstree:min_max_p(T3)),
+
+    ok.
+
+%-----------------------------------------------------------------------------------------------
+
+example(0) -> [1, 2, 3, [4, [-5]], 6];
+
+example(1) ->
+[
+    [
+        [
+            [5,6],
+            [7,4,5]
+        ],
+        [
+            [3]
+        ]
+    ],
+    [
+        [
+            [6],
+            [6,9]
+        ],
+        [
+            [7]
+        ]
+    ],
+    [
+        [
+            [5]
+        ],
+        [
+            [9,8],
+            [6]
+        ]
+    ]
+]; 
+
+% example from http://web.cs.ucla.edu/~rosen/161/notes/alphabeta.html
+example(2) ->
+[
+  [[[3, 17], [2, 12]], [[15],  [25, 0]]],
+  [[[2, 5], [3]], [[2, 14]]]
+];
+
+% example from http://people.cs.pitt.edu/~litman/courses/cs2710/lectures/pruningReview.pdf
+example(3) ->
+[
+    [[4, 3], [6, 2]],
+    [[2, 1], [9, 5], [3, 1]],
+    [[5, 4], [7, 5]]
+].
+
+%-------------------------------------------------------------------------------------------------
