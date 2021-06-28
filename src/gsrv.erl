f173eb93eff4a76afd54af8c41f8a3ae4322d37e
Anatoly Rodionov <arodionov@nexvortex.com>
2021-06-27 22:40:25 -0400

    simple implementation of gen_server multi call added

---
+++ src/gsrv.erl
@@ -0,0 +1,115 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%
+%%% @author Anatoly Rodionov <anatoly.ya.rodionov@gmail.com>
+%%% @copyright 2021 Anatoly Rodionov
+%%%
+%%% @doc This is a simple implementation of gen_server multi call.
+%%% <b>Requires Erlang/OTP 24</b>.
+%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-module(gsrv).
+
+%% API exports
+-export([multicall1/1, multicall2/1, multicall3/1, multicall4/1, multicall4/2]).
+
+-define(DEFAULT_TIMEOUT, 5000).  % gen_server:call default timeout
+
+-define(CURRENT_TIME, erlang:system_time(milli_seconds)).
+
+-spec multicall1([{pid(), term()} | {pid(), term(), non_neg_integer() | infinity}]) -> 
+  [term()].
+% @doc Naive implementation; makes gen_server calls one after another.
+% Execution time is proportional to  sum of all call times.
+% 
+% This not solution. This function is for illustration and comparison.
+% 
+% @param CallRequests list of call requests
+% @returns list of calls responses in the same order as calls
+multicall1(CallRequests) ->
+    lists:map(fun 
+      ({Pid, Call})          -> catch gen_server:call(Pid, Call);
+		  ({Pid, Call, Timeout}) -> catch gen_server:call(Pid, Call, Timeout)
+	    end,  CallRequests).
+
+-spec multicall2([{pid(), term()} | {pid(), term(), non_neg_integer() | infinity}]) -> 
+  [{reply, term()} | timeout | {error, term()}].
+% @doc Makes asynchronous gen_server calls.
+% First asynchronously sends all requests using gen_server:send_request. 
+% Then gen_server:receive_response receives results.
+% In this case calls to different recipients are executed concurrently
+% by their servers.
+% 
+% Execution time may be proportional to time of the longest call.
+% Let N be the the CallRequests length and M - number of different servers;
+% n(i) - number of calls to server(i), t(i,j) - execution time of 
+% call(i, j) to service(i). Here 0 &lt; i &lt;= N, 0 &lt; j &lt;= n(i). 
+% Let t(i) be the sum of all t(i,j). Expectation time of multicall2 call
+% is evidently max(t(i)).
+% 
+% In the worst case scenario when N=1 multicall2 is no better than multicall1,
+% but when N=M then execution time may be proportional to time of the longest call.
+% 
+% The other worst case scenario is when all recipients never answer. 
+% In this case full execution time is sum of all timeouts.
+% 
+% @param CallRequests list of call requests
+% @returns list of receive_response return values  in the same order as calls
+multicall2(CallRequests) ->
+    CallRefs = lists:map(fun 
+      ({Pid, Call})          -> {gen_server:send_request(Pid, Call), ?DEFAULT_TIMEOUT};
+		  ({Pid, Call, Timeout}) -> {gen_server:send_request(Pid, Call), Timeout}
+		  end, CallRequests),
+    lists:map(fun ({Ref, Tout}) -> gen_server:receive_response(Ref, Tout) end, CallRefs).
+
+-spec multicall3([{pid(), term()} | {pid(), term(), non_neg_integer() | infinity}]) -> 
+  [{reply, term()} | timeout | {error, term()}].
+% @doc Same as multicall2 but treats timeouts differently - it
+% calculates time for each call starting from the last call.
+% 
+% Problem of multicall2 is that it changes timeout semantic.
+% It interprets timeout as time of the time started from receive_response call,
+% not from the time of send_request call. multicall3 fixes this by using time 
+% the last send_request as the starting time for each receive_response.
+% 
+% multicall3 not only fixes timeout semantic but also makes the second worst
+% scenario (all recipient do not answer in time). multicall3 will wait only
+% maximum of timeout times.
+% 
+% There is still reason for using multicall2 - it gives you maximum chances to
+% to receive answers from recipients.
+% 
+% @param CallRequests list of call requests
+% @returns list of receive_response return values in the same order as calls
+multicall3(CallRequests) ->
+    CallRefs = lists:map(fun 
+      ({Pid, Call})          -> {gen_server:send_request(Pid, Call), ?DEFAULT_TIMEOUT};
+		  ({Pid, Call, Timeout}) -> {gen_server:send_request(Pid, Call), Timeout}
+		  end, CallRequests),
+    StartTime = ?CURRENT_TIME,
+    lists:map(fun 
+      ({Ref, infinity}) -> gen_server:receive_response(Ref, infinity);
+      ({Ref, Tout})     -> gen_server:receive_response(Ref, max(0, StartTime + Tout - ?CURRENT_TIME))
+      end, CallRefs).
+% 
+-spec multicall4([{pid(), term()}]) -> 
+  [{reply, term()} | timeout | {error, term()}].
+% @doc Same as multicall4(CallRequests, ?DEFAULT_TIMEOUT)
+% @param CallRequests list of call requests
+% @returns list of receive_response return values
+multicall4(CallRequests) ->  multicall4(CallRequests, ?DEFAULT_TIMEOUT).
+
+-spec multicall4([{pid(), term()}], non_neg_integer() | infinity) -> 
+  [{reply, term()} | timeout | {error, term()}].
+% @doc Same as multicall2 but another timeout semantic; 
+% timeout is understood as time limit for all calls to be completed.
+% 
+% @param CallRequests list of call requests
+% @param Timeout is time limit for all calls to be completed
+% @returns list of receive_response return values  in the same order as calls
+multicall4(CallRequests, infinity) ->
+    CallRefs = [gen_server:send_request(Pid, Call) || {Pid, Call} <- CallRequests],
+    [gen_server:receive_response(Ref, infinity) || Ref <- CallRefs];
+multicall4(CallRequests, Timeout) ->
+    CallRefs = [gen_server:send_request(Pid, Call) || {Pid, Call} <- CallRequests],
+    EndTime = ?CURRENT_TIME + Timeout,
+    [gen_server:receive_response(Ref, max(0, EndTime - ?CURRENT_TIME)) || Ref <- CallRefs].
---
+++ src/test_srv.erl
@@ -0,0 +1,154 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Copyright (c) 2021 Anatoly Rodionov.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%
+%%% @author Anatoly Rodionov <anatoly.ya.rodionov@gmail.com>
+%%% @copyright 2021 Anatoly Rodionov
+%%%
+%%% @doc This is a simple server for testing purposes
+%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%-----------------------------------------------------------------------------------------------
+
+-module(test_srv).
+
+-behavior(gen_server).
+
+-vsn(1.0).
+
+%-----------------------------------------------------------------------------------------------
+-export([init/1, start/1, start_link/1]).
+
+-export([handle_call/3]).
+
+-export([get/1]).
+
+-export([mstart/1, time_test/1, time_test/2]).
+
+%-----------------------------------------------------------------------------------------------
+-record(s_t, {sleep_time = 0  :: non_neg_integer()}).
+
+-type s_t() :: #s_t{}.
+
+%-----------------------------------------------------------------------------------------------
+% Interfaces
+%-----------------------------------------------------------------------------------------------
+
+-spec start(non_neg_integer()) -> {ok, pid()}.
+
+% @doc Starts {@module}.
+start(SleepTime) ->
+    Name = list_to_atom(integer_to_list(SleepTime)),
+    gen_server:start({local, Name}, ?MODULE, SleepTime, []).
+
+-spec start_link(non_neg_integer()) -> {ok, pid()}.
+
+% @doc Starts {@module}.
+start_link(SleepTime) ->
+    Name = list_to_atom(integer_to_list(SleepTime)),
+    gen_server:start_link({local, Name}, ?MODULE, SleepTime,
+			  []).
+
+-spec init(non_neg_integer()) -> {ok, s_t()}.
+
+% @doc Inits {@module}.
+init(SleepTime) -> {ok, #s_t{sleep_time = SleepTime}}.
+
+%-----------------------------------------------------------------------------------------------
+
+-spec get(atom() |
+	  non_neg_integer()) -> non_neg_integer().
+get(Name) when is_integer(Name) ->
+    (?MODULE):get(list_to_atom(integer_to_list(Name)));
+get(Name) when is_atom(Name) ->
+    gen_server:call(Name, sleep_time).
+
+%-----------------------------------------------------------------------------------------------
+% Callbacks
+%-----------------------------------------------------------------------------------------------
+
+handle_call(sleep_time, _From, ST) ->
+    timer:sleep(ST#s_t.sleep_time),
+    {reply, ST#s_t.sleep_time, ST}.
+
+%-----------------------------------------------------------------------------------------------
+
+% L = [111,222,333,444,555,666,777,888,999].
+% P = lists:map(fun(N) -> {ok, P} = test_srv:start(N), {P, sleep_time} end, L).
+% [test_srv:get(N) || N <- L] .
+% timer:tc(fun(Lst) -> lists:foreach(fun(N) -> test_srv:get(N) end, Lst) end, [L]).
+% lists:sum(L).
+% lists:max(L).
+
+% {ok, P314} =test_srv:start(314).
+% gen_server:call('314', sleep_time).
+% gen_server:call(P314, sleep_time).
+% R = gen_server:send_request(P314, sleep_time).
+
+
+-spec mstart([non_neg_integer() | {non_neg_integer(), non_neg_integer() | infinity}]) ->
+  [{pid(), sleep_time} | {pid(), sleep_time, non_neg_integer() | infinity}].
+mstart([]) -> [];
+mstart([{N, Timeout} | Tail]) -> [{save_start(N), sleep_time, Timeout} | mstart(Tail)];
+mstart([N | Tail]) -> [{save_start(N), sleep_time} | mstart(Tail)].
+
+
+-spec save_start(non_neg_integer()) -> pid().
+save_start(N) when is_integer(N) andalso N >= 0 ->
+  case test_srv:start(N) of 
+    {ok, P} -> P;
+    {error, {already_started, P}} -> P
+  end.
+
+-spec time_test
+  ([{pid(), sleep_time} | {pid(), sleep_time, non_neg_integer() | infinity}]) ->
+    [{atom(), non_neg_integer()}].
+time_test(PLst) -> time_test(PLst, [multicall1, multicall2, multicall3, multicall4]).
+
+-spec time_test
+  ([{pid(), sleep_time} | {pid(), sleep_time, non_neg_integer() | infinity}], atom() | [atom()]) ->
+    [{atom(), non_neg_integer()}].
+time_test(PLst, Call) when is_atom(Call)->
+  {T, _} = timer:tc(gsrv, Call, [PLst]),
+  {Call, T/100};
+time_test(PLst, Calls) -> [time_test(PLst, Call) || Call <- Calls].
+
+%-----------------------------------------------------------------------------------------------
+% 30> test_srv:time_test(test_srv:mstart([23,34,45,56,67]), multicall4).
+% {multicall4,674.64}
+% 32> test_srv:time_test(test_srv:mstart([2300,3400,4500,5600,6700])).            
+% [{multicall1,202044.53},
+%  {multicall2,84010.1},
+%  {multicall3,50008.07},
+%  {multicall4,50009.45}]
+% 33> test_srv:time_test(test_srv:mstart([10000])).                   
+% [{multicall1,50009.19},
+%  {multicall2,50009.73},
+%  {multicall3,50009.36},
+%  {multicall4,50010.89}]
+% 34> test_srv:time_test(test_srv:mstart([10000, 10000, 10000])).
+% [{multicall1,150028.99},
+%  {multicall2,150026.77},
+%  {multicall3,50012.69},
+%  {multicall4,50008.29}]
+% 35>  test_srv:time_test(test_srv:mstart([230,340,450,560,670,760,870])).            
+% [{multicall1,38869.64},
+%  {multicall2,8708.96},
+%  {multicall3,8710.1},
+%  {multicall4,8710.14}]
+%-----------------------------------------------------------------------------------------------
